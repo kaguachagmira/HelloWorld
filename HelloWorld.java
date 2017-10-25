ce9e63d8a8fac4c61152a28d1797ba54d4fb658b
kaguachagmira <33032880+kaguachagmira@users.noreply.github.com>
2017-10-25 12:00:04 -0500

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Kevin");
-		
+		Chicken chicken;
+		chicken = new Chicken();
+		chicken.initialize("lucy","cafe",2);
+		chicken.showData();
 	}
-	
 }
