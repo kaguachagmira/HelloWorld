717ac15fa547ea2060ed08680aad7a65df1888c4
kaguachagmira <mantillagka@hotmail.com>
2017-10-25 22:19:35 -0500

    V2.0thechicken

--- Chicken.java
+++ Chicken.java
@@ -5,9 +5,11 @@ public class Chicken
 	//Atributos con Lower Camel Case
 	//Metodos con Lower Camel Case
 	//Variables con Lower Camel Case
+		// Atributos
 		private String name;
 		private String color;
 		private int  age;
+		//Metodos
 		public void initialize(String name, String color, int age)
 		{
 			this.name=name; //el primero es el atributo de la clase el segundo es el parametro que recibo
@@ -16,19 +18,19 @@ public class Chicken
 		}
 		public void layAndEgg()
 		{
-			System.out.println("The Chicken lay an egg");
+			System.out.println("The Chicken Lucia lay an egg");
 		}
 		public void poop()
 		{
-			System.out.println("The Chicken pooped");
+			System.out.println("The Chicken Maria pooped");
 		}
 		public void drink()
 		{
-			System.out.println("The Chicken drank");
+			System.out.println("The Chicken Marta drank");
 		}
 		public void showData()
 		{
-			System.out.println("The Name of the Chiken is: " + name + "Color: " + color + "Age: " + age);
+			System.out.println("The Name of the Chicken is: " + name + " Color: " + color + " Age: " + age);
 		}
 
 	
--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,26 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Kevin");
-		Chicken chicken;
-		chicken = new Chicken();
-		chicken.initialize("lucy","cafe",2);
+		
+		Chicken chicken = new Chicken();
+		chicken.initialize("Lucy","brown",2);
 		chicken.showData();
+		
+		Chicken chicken1 = new Chicken();
+		chicken1.initialize("Lucia","yellow",1);
+		chicken1.showData();
+		chicken1.layAndEgg();
+		
+		Chicken chicken2 = new Chicken();
+		chicken2.initialize("Maria","white",2);
+		chicken2.showData();
+		chicken2.poop();
+		
+		Chicken chicken3 = new Chicken();
+		chicken3.initialize("Marta","white",3);
+		chicken3.showData();
+		chicken3.drink();
+		
+			
 	}
 }
